260575144e42bd7eb95b9b45fb16f2abb44fe4c4
thinktwisted <65298630+thinktwisted@users.noreply.github.com>
2020-06-29 14:37:09 -0400

    Create IdleBuster.py
    Basic script to keep your computer from going idle.

---
+++ IdleBuster.py
@@ -0,0 +1,45 @@
+import win32api, time
+import pyautogui as gui
+
+def getIdleTime():
+    '''A function to return the idle time of the PC in seconds'''
+    return (win32api.GetTickCount() - win32api.GetLastInputInfo())/1000
+
+run = True
+validInput = False
+
+while not validInput:
+    idleTime = (gui.prompt('What is your idle time [1-60 minutes]?'))
+    try:
+        int(idleTime)
+        break;
+    except ValueError:
+        continue
+    except TypeError:
+        exit()
+
+idleTime = int(idleTime)*60
+wiggleCount = 0
+print('Target time [sec]: ' + str(idleTime))
+
+while run:
+    currentTime = getIdleTime()
+    #print("Idle time [sec]: " + str(currentTime))
+    if (currentTime) > idleTime:
+        currentPosition = gui.position()
+        gui.moveTo(currentPosition[0]+50, currentPosition[1]+50, duration=1)
+        gui.moveTo(currentPosition[0], currentPosition[1], duration=1)
+        print('Mouse just wiggled, idle time was %s' % str(currentTime))
+        wiggleCount += 1
+        try:
+            for i in range(idleTime-5):
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print('Exiting, total wiggles: %s' % str(wiggleCount))
+            run = False
+    else:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print('Exiting, total wiggles: %s' % str(wiggleCount))
+            run = False

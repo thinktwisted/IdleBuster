677aa4186e5478b60efd5dd1e2b7408d503350e4
thinktwisted <65298630+thinktwisted@users.noreply.github.com>
2020-07-08 13:21:39 -0400

    Update IdleBuster.py
    Added python3 and comments; changed script to look for timeout minus 1 second to ensure we don't miss it; changed a few of the printouts to make more sense

--- IdleBuster.py
+++ IdleBuster.py
@@ -1,3 +1,5 @@
+#! python3
+# IdleBuster.py - a simple program to keep your computer wake
 import win32api, time
 import pyautogui as gui
 
@@ -5,8 +7,8 @@ def getIdleTime():
     '''A function to return the idle time of the PC in seconds'''
     return (win32api.GetTickCount() - win32api.GetLastInputInfo())/1000
 
-run = True
 validInput = False
+run = True
 
 while not validInput:
     idleTime = (gui.prompt('What is your idle time [1-60 minutes]?'))
@@ -20,25 +22,25 @@ while not validInput:
 
 idleTime = int(idleTime)*60
 wiggleCount = 0
-print('Target time [sec]: ' + str(idleTime))
+print('Idle time [sec]: ' + str(idleTime))
 
 while run:
     currentTime = getIdleTime()
-    if (currentTime) > idleTime:
+    if (currentTime) > idleTime-1:
         for i in range(2):
             gui.press('shift')
             time.sleep(1)
         print('Shift pressed idle time was %s' % str(currentTime))
         wiggleCount += 1
         try:
-            for i in range(idleTime-5):
+            for i in range(idleTime-2):
                 time.sleep(1)
         except KeyboardInterrupt:
-            print('Exiting, total wiggles: %s' % str(wiggleCount))
+            print('Exiting, total idles avoided: %s' % str(wiggleCount))
             run = False
     else:
         try:
             time.sleep(1)
         except KeyboardInterrupt:
-            print('Exiting, total presses: %s' % str(wiggleCount))
+            print('Exiting, total idles avoided: %s' % str(wiggleCount))
             run = False
